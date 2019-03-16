0434dd20c0bf3cdd92dd21d7750174322e8994f5
Jobert Abma <jobert@hackerone.com>
2019-03-16 12:26:03 -0700

    Automatically calculate listener template size

--- src/generators/ListenerTemplate.js
+++ src/generators/ListenerTemplate.js
@@ -1,14 +1,13 @@
 class ListenerTemplate {
   static generate(func) {
     // url should be dynamic
-    // 13 should be dynamic
-    return `
+    let script = `
     <script>
       window.onerror = function(error, _url, line) {
         window.parent.postMessage({
           function: 'errorHandler',
           error: error,
-          line: line - 16
+          line: line - {number_of_lines}
         }, 'http://localhost:3000');
       }
       window.alert = function(value) { }
@@ -18,7 +17,9 @@ class ListenerTemplate {
         window.parent.postMessage({ function: '${func}', value: value }, 'http://localhost:3000');
       };
     </script>
-    `
+    `;
+
+    return script.replace('{number_of_lines}', script.split("\n").length);
   }
 }
 
