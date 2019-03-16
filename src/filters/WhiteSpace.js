b4a238675d5283acf530a5460b10857baf727a99
Jobert Abma <jobert@hackerone.com>
2019-03-16 10:43:01 -0700

    Implement a whitespace filter

---
+++ src/filters/WhiteSpace.js
@@ -0,0 +1,13 @@
+import _ from 'underscore';
+
+class WhiteSpace {
+  process(payload) {
+    return payload.replace(/\s+/g, '');
+  }
+
+  static generate(_filters) {
+    return new this();
+  }
+}
+
+export default WhiteSpace;
