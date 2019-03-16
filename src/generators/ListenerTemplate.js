d8c599d0d4562dfad9b8bcf7e881cc3c5cb6c4c6
Jobert Abma <jobert@hackerone.com>
2019-03-16 12:23:46 -0700

    Overwrite blocking JS functions

--- src/generators/ListenerTemplate.js
+++ src/generators/ListenerTemplate.js
@@ -8,9 +8,12 @@ class ListenerTemplate {
         window.parent.postMessage({
           function: 'errorHandler',
           error: error,
-          line: line - 13
+          line: line - 16
         }, 'http://localhost:3000');
       }
+      window.alert = function(value) { }
+      window.confirm = function(value) { return false }
+      window.prompt = function(text, defaultText = '') { return '' }
       window.${func} = function(value) {
         window.parent.postMessage({ function: '${func}', value: value }, 'http://localhost:3000');
       };
