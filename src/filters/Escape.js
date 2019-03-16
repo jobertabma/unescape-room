fd5f8cc4541cf90f50f87178fc33edf6d07f2c53
Jobert Abma <jobert@hackerone.com>
2019-03-16 11:41:08 -0700

    EscapeFilter > Escape

--- src/filters/Escape.js
+++ src/filters/Escape.js
@@ -1,12 +1,14 @@
-class EscapeFilter {
+class Escape {
   process(payload) {
+    // should it also escape backticks?
     return payload.replace(/(['"\\])/g, '\\$1');
   }
 
   static generate(_filters) {
     // should pick at least one char to escape
+    // or a number of characters
     return new this();
   }
 }
 
-export default EscapeFilter;
+export default Escape;
